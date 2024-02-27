5cca35f8e5e32310de7fbf148e06e1fdc54b569d
Tan Quang DUONG <tan.duong@quant.global>
2024-02-27 15:32:09 +0100

    update presentation implementing

--- template.py
+++ template.py
@@ -1,36 +1,20 @@
-import sys
-sys.path.append('../')
-import torch
-from transformer.layer import MultiHeadAttention, FeedForward, LayerNorm
-from transformer.encoder import EncoderLayer, Encoder
+print("decoder_input_ids:", decoder_input_ids)
+print("decoder_input_ids shape:", decoder_input_ids.shape)
+print("\n")
 
-# Define configuration
-d_model = 6 # feature dimension
-h = 3 #  number of heads
-dropout = 0.1 # dropout ratio
-d_ff = 2048 # the dimension of the feed forward network
-batch = 1 # batch size
-seq_len = 4 # sequence length
+# Create padding mask
+padding_mask = create_padding_mask(decoder_input_ids, pad_token_id) # (1, seq_len)
+print("padding_mask: \n", padding_mask)
+print("padding_mask shape:", padding_mask.shape)
+print("\n")
 
-num_layers = 3 # number of encoder layer
+# Create a causal mask 
+causal_mask = create_causal_mask(seq_len)
+print('causal_mask: \n', causal_mask)
+print('causal_mask shape:', causal_mask.shape)
+print("\n")
 
-# Create an instance of the MultiHeadAttention and FeedForward classes
-self_attention_engine = MultiHeadAttention(d_model, h, dropout)
-feed_forward = FeedForward(d_model, d_ff, dropout)  
-
-# Create an instance of the EncoderLayer class
-encoder_layer = EncoderLayer(d_model, self_attention_engine, feed_forward, dropout)
-
-# Create an instance of the Encoder class
-encoder = Encoder(d_model, encoder_layer, num_layers)
-
-# Create a random tensor to represent a batch of sequences
-torch.manual_seed(68) # for reproducible result of random process
-x = torch.rand(batch, seq_len, d_model)  
-
-# Pass the tensor through the encoder
-output = encoder(x)
-
-print("Initial input tensor: \n", x)
-print("Encoder Output: \n", output)
-print("Encoder Output's shape: \n", output.shape)  
+# Create decoder mask
+decoder_mask = create_decoder_mask(decoder_input_ids, pad_token_id, seq_len)
+print('decoder_mask: \n', decoder_mask)
+print('decoder_mask shape:', decoder_mask.shape)
